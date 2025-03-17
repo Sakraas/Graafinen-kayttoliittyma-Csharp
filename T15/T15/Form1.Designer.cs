5e98d4a67d0d9071a07b846b9e051d55fc3950dd
Sakari Raassina <sakari.raassina@edu.keuda.fi>
2025-03-17 09:30:05 +0200

    Add files via upload
    T15

---
+++ T15/T15/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace T15
+{
+    partial class SekuntikelloForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimerLB = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.KaynnistaBT = new System.Windows.Forms.Button();
+            this.LopetaBT = new System.Windows.Forms.Button();
+            this.TyhjennaBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TimerLB
+            // 
+            this.TimerLB.AutoSize = true;
+            this.TimerLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimerLB.Location = new System.Drawing.Point(13, 13);
+            this.TimerLB.Name = "TimerLB";
+            this.TimerLB.Size = new System.Drawing.Size(410, 73);
+            this.TimerLB.TabIndex = 0;
+            this.TimerLB.Text = "00:00:00.000";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // KaynnistaBT
+            // 
+            this.KaynnistaBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KaynnistaBT.Location = new System.Drawing.Point(3, 89);
+            this.KaynnistaBT.Name = "KaynnistaBT";
+            this.KaynnistaBT.Size = new System.Drawing.Size(111, 91);
+            this.KaynnistaBT.TabIndex = 1;
+            this.KaynnistaBT.Text = "Käynnistä";
+            this.KaynnistaBT.UseVisualStyleBackColor = true;
+            this.KaynnistaBT.Click += new System.EventHandler(this.KaynnistaBT_Click);
+            // 
+            // LopetaBT
+            // 
+            this.LopetaBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LopetaBT.Location = new System.Drawing.Point(120, 89);
+            this.LopetaBT.Name = "LopetaBT";
+            this.LopetaBT.Size = new System.Drawing.Size(192, 91);
+            this.LopetaBT.TabIndex = 2;
+            this.LopetaBT.Text = "Lopeta";
+            this.LopetaBT.UseVisualStyleBackColor = true;
+            this.LopetaBT.Click += new System.EventHandler(this.LopetaBT_Click);
+            // 
+            // TyhjennaBT
+            // 
+            this.TyhjennaBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TyhjennaBT.Location = new System.Drawing.Point(318, 89);
+            this.TyhjennaBT.Name = "TyhjennaBT";
+            this.TyhjennaBT.Size = new System.Drawing.Size(104, 91);
+            this.TyhjennaBT.TabIndex = 3;
+            this.TyhjennaBT.Text = "Tyhjennä";
+            this.TyhjennaBT.UseVisualStyleBackColor = true;
+            this.TyhjennaBT.Click += new System.EventHandler(this.TyhjennaBT_Click);
+            // 
+            // SekuntikelloForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(428, 185);
+            this.Controls.Add(this.TyhjennaBT);
+            this.Controls.Add(this.LopetaBT);
+            this.Controls.Add(this.KaynnistaBT);
+            this.Controls.Add(this.TimerLB);
+            this.Name = "SekuntikelloForm";
+            this.Text = "Sekuntikello";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TimerLB;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button KaynnistaBT;
+        private System.Windows.Forms.Button LopetaBT;
+        private System.Windows.Forms.Button TyhjennaBT;
+    }
+}
+
---
+++ T15/T15/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace T15
+{
+    public partial class SekuntikelloForm : Form
+    {
+        private Stopwatch T15 = new Stopwatch();
+        public SekuntikelloForm()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimerLB.Text = String.Format("{0:hh\\:mm\\:ss\\:fff}", T15.Elapsed);
+        }
+
+        private void KaynnistaBT_Click(object sender, EventArgs e)
+        {
+            T15.Start();
+        }
+
+        private void LopetaBT_Click(object sender, EventArgs e)
+        {
+            T15.Stop();
+        }
+
+        private void TyhjennaBT_Click(object sender, EventArgs e)
+        {
+            T15.Reset();
+        }
+    }
+}
